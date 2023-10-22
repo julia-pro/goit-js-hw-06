0e1c271f55a0f47b9a60bd0d0b76c1f676c677f9
julia-pro <juliaszatkowska52@gmail.com>
2023-10-22 20:11:14 +0200

    Add code

---
+++ js/task-01.js
@@ -0,0 +1,73 @@
+// Завдання 1
+// HTML містить список категорій ul#categories.
+
+// <ul id="categories">
+//   <li class="item">
+//     <h2>Animals</h2>
+//     <ul>
+//       <li>Cat</li>
+//       <li>Hamster</li>
+//       <li>Horse</li>
+//       <li>Parrot</li>
+//     </ul>
+//   </li>
+//   <li class="item">
+//     <h2>Products</h2>
+//     <ul>
+//       <li>Bread</li>
+//       <li>Prasley</li>
+//       <li>Cheese</li>
+//     </ul>
+//   </li>
+//   <li class="item">
+//     <h2>Technologies</h2>
+//     <ul>
+//       <li>HTML</li>
+//       <li>CSS</li>
+//       <li>JavaScript</li>
+//       <li>React</li>
+//       <li>Node.js</li>
+//     </ul>
+//   </li>
+// </ul>
+
+// Напиши скрипт, який:
+
+// Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+// Для виконання цього завдання потрібно використати метод forEach() і властивості навігації по DOM.
+
+// В результаті, в консолі будуть виведені наступні повідомлення.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+// Знайти елемент ul#categories.
+const categoriesList = document.querySelector('#categories');
+
+// Знайти всі елементи li.item в ul#categories.
+const categoryItems = categoriesList.querySelectorAll('li.item');
+
+// Вивести кількість категорій в ul#categories.
+console.log(`Number of categories: ${categoryItems.length}`);
+
+// Пройтись по кожному елементу li.item та вивести текст заголовку і кількість елементів в категорії.
+categoryItems.forEach(categoryItem => {
+  const categoryTitle = categoryItem.querySelector('h2').textContent;
+  const categoryElements = categoryItem.querySelectorAll('ul li').length;
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${categoryElements}`);
+});
+
+
+
+
+
